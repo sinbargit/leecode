cf1a93d6bced4ca76768d91c49f92b5705c677f7
sinbarxie <sinbar.xing@outlook.com>
2019-10-17 21:30:44 +0800

    add

---
+++ MinStack.js
@@ -0,0 +1,67 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+	this.stack = [];   
+	this.min = null;
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+	if(this.min === null) {
+		this.min = x;
+		this.stack.push(x)
+	}else
+	{
+		if(this.min > x)
+		{
+			this.stack.push(this.min)	
+			this.stack.push(x)
+			this.min = x
+		}
+		else{
+			this.stack.push(x)
+		}
+	}
+
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+	if(this.min === this.top())
+	{
+		this.pop();
+		this.min = this.pop();
+	}
+	else{
+		this.pop();	
+	}
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+	return this.stack[this.stack.length-1] 
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+	return this.min 
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
