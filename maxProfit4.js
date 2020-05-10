edfd0ff2c69e3e5eaa5bbaaf814602f28e7b63fc
sinbar <sinbar.xing@outlook.com>
2020-05-10 22:14:53 +0800

    Merge branch 'master' of github.com:sinbargit/leecode
    # Conflicts:
    #	rob2.js

--- maxProfit4.js
+++ maxProfit4.js
@@ -4,23 +4,43 @@
  * @return {number}
  */
 var maxProfit = function (k, prices) {
+    if(k>=prices.length){
+        var acc = 0,i=0
+        while(i<prices.length-1)
+        {
+            if(prices[i+1]>prices[i])
+            {
+                acc +=(prices[i+1]-prices[i])
+            }
+            i++
+        }
+        return acc
+
+    }
     var dp = new Array(prices.length).fill('').map(() => {
-        return new Array(k).fill('').map(() => {
-            return new Array(2).fill('');
-        });
+       return new Array(k+1).fill('').map(() => {
+           return new Array(2).fill('');
+       });
     });
+    if(dp.length === 0){return 0}
     var i = 1;
-    dp[0][0][0] = 0
-    dp[0][0][1] = -prices[0]
     while (i < prices.length) {
-        var _k = 1;
-        while (_k < k) {
+        dp[i][0][0] = 0
+        dp[i][0][1] = Number.MAX_SAFE_INTEGER
+        var _k = k;
+        while (_k >= 1) {
+            dp[0][_k][0] = 0
+            dp[0][_k][1] = -prices[0]
+            _k--
+        }
+        _k = k
+        while (_k >= 1) {
             dp[i][_k][0] = Math.max(dp[i-1][_k][0],dp[i-1][_k][1]+prices[i])
             dp[i][_k][1] = Math.max(dp[i-1][_k][1],dp[i-1][_k-1][0]-prices[i])
-            _k++
+            _k--
         }
         i++;
     }
-    return dp[i-1][k-1][0]
+    return dp[i-1][k][0]
 };
 console.log(maxProfit(2, [3, 3, 5, 0, 0, 3, 1, 4]));
