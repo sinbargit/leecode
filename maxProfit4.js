7275d5e58a2ac9f75698b52d9222c29cd45142ca
sinbar <sinbar.xing@outlook.com>
2020-05-11 09:38:03 +0800

    dd

--- maxProfit4.js
+++ maxProfit4.js
@@ -5,22 +5,27 @@
  */
 var maxProfit = function (k, prices) {
     var dp = new Array(prices.length).fill('').map(() => {
-        return new Array(k).fill('').map(() => {
-            return new Array(2).fill('');
+        return new Array(k + 1).fill('').map(() => {
+            return new Array(2).fill('*');
         });
     });
     var i = 1;
-    dp[0][0][0] = 0
-    dp[0][0][1] = -prices[0]
+    if (dp.length === 0) return 0;
+    dp[0][k][0] = 0;
+    dp[0][k][1] = -prices[0];
+    dp[0][k-1][0] = 0;
+    dp[0][k-1][1] = -prices[0];
     while (i < prices.length) {
-        var _k = 1;
-        while (_k < k) {
-            dp[i][_k][0] = Math.max(dp[i-1][_k][0],dp[i-1][_k][1]+prices[i])
-            dp[i][_k][1] = Math.max(dp[i-1][_k][1],dp[i-1][_k-1][0]-prices[i])
-            _k++
+        var _k = k;
+        while (_k >= 1) {
+            console.log(dp[i - 1][_k][0],dp[i - 1][_k][1],dp[i - 1][_k][1],dp[i - 1][_k - 1][0])
+            dp[i][_k][0] = Math.max(dp[i - 1][_k][0], dp[i - 1][_k][1] + prices[i]);
+            dp[i][_k][1] = Math.max(dp[i - 1][_k][1], dp[i - 1][_k - 1][0] - prices[i]);
+            _k--;
         }
         i++;
     }
-    return dp[i-1][k-1][0]
+    return dp[i - 1][k][0];
 };
-console.log(maxProfit(2, [3, 3, 5, 0, 0, 3, 1, 4]));
+
+console.log(maxProfit(2, [2, 1, 2, 0, 1]));
