0e7920c1a26e208899e5ec15d9d5d2c0efb5347a
sinbar <sinbar.xing@outlook.com>
2020-05-12 21:10:02 +0800

    Merge branch 'master' of github.com:sinbargit/leecode
    # Conflicts:
    #	maxProfit4.js

---
+++ isMatch2.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+    let si = 0, pi = 0;
+
+    function match(si, pi) {
+        if (si >= s.length && pi >= p.length) {
+            return true;
+        }
+        let pic = p.charAt(pi);
+        let sic = s.charAt(si);
+        let pic2 = p.charAt(pi + 1);
+        if (si <= s.length && (pic === sic || pic === '.')) {
+            if (pic2 === '*') {
+                return match(si + 1, pi) || match(si + 1, pi + 2) || match(si, pi + 2);
+            }
+            return match(si + 1, pi + 1);
+        }
+        else if (pic2 === '*') {
+            return match(si, pi + 2);
+        }
+        else {
+            return false;
+        }
+    }
+
+    return match(0, 0);
+};
+console.log(isMatch('aa', 'a'));
+console.log(isMatch('aa', 'a*'));
+console.log(isMatch('ab', '.*'));
+console.log(isMatch('aab', 'c*a*b'));
+console.log(isMatch('mississippi', 'mis*is*p*.'));
