2b8f2816384dae83201595e787e27678f6f998d8
sinbarxie <sinbar.xing@outlook.com>
2020-06-02 14:05:09 +0800

    ad

--- minScoreTriangulation.js
+++ minScoreTriangulation.js
@@ -3,28 +3,38 @@
  * @return {number}
  */
 var minScoreTriangulation = function(A) {
-	const l = A.length	
-	const dp = new Array(l).fill('').map(()=>{
-		return new Array(l).fill('')
+	const l = A.length - 1
+	const dp = new Array(A.length).fill('').map(()=>{
+		return new Array(A.length).fill('')
 	})
 	let i = 0;
-	while(++i<l)
-	{
-		dp[i][i+1] = 0
-	}
-	i=0
 	while(i<l)
 	{
-		let j = l;
-		while(i<j)
-		{
-			
-		}
-		let m = i+1
-		dp[i][m] = 0
-		while(m<j)
-		{
-			dp[i][j] = dp[i][m]	
-		}
+		dp[i][i+1] = 0
+		i++
 	}
+	console.log(dp);
+	i=l
+	while(i>=0)
+	{
+		let j = i+2;
+		while(j<=l)
+		{
+			let min = Number.MAX_SAFE_INTEGER
+			let m = i+1
+			console.log(i,j,m)
+			while(m<j)
+			{
+				min = Math.min(dp[i][m]+A[i]*A[j]*A[m]+dp[m][j],min)
+				m++
+			}
+			dp[i][j] = min
+			j++
+		}
+		i--
+	}
+	console.log(dp)
+	return dp[0][l]
 };
+console.log(minScoreTriangulation([3,7,4,5]))
+console.log(minScoreTriangulation([1,3,1,4,1,5]))
